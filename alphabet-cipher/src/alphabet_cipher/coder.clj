826b7b9eb81b8167c4694ebcfdf6c5d80f3d9ad9
Howard Fan <Howard_Fan@jabil.com>
2015-02-08 17:26:14 +0800

    better fn naming

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -4,29 +4,29 @@
 (def codebook "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")
 (count (distinct codebook))
 
-(defn -secret
+(defn take-rep
   "take first n chars by repeating k many times as necessary"
   [n k]
   (take n (flatten (repeat (seq k)))))
 
-(-secret 5 "ab")
+(take-rep 5 "ab")
 
 (defn index-a [c]
   (- (int c) (int \a))) ; CLJ
-  ;(- (.charCodeAt c) (.charCodeAt \a))) ; CLJS
+;(- (.charCodeAt c) (.charCodeAt \a))) ; CLJS
 
-(defn -encode [yes secret message]
+(defn codec [encode secret message]
   (let [n (count message)
-        secret (-secret n secret)]
-  (for [i (range n)
-      :let [m (index-a (nth message i))
-            s (index-a (nth secret i))
-            k (if yes (+ s m) (- (+ m 26) s))]]
-        (nth codebook k))))
+        secret (take-rep n secret)]
+    (for [i (range n)
+          :let [m (index-a (nth message i))
+                s (index-a (nth secret i))
+                k (if encode (+ s m) (- (+ m 26) s))]]
+      (nth codebook k))))
 
 (defn encode [secret message]
-  (string/join (-encode true secret message)))
+  (string/join (codec true secret message)))
 
 (defn decode [secret message]
-  (string/join (-encode false secret message)))
+  (string/join (codec false secret message)))
 
