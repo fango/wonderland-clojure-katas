e917c2803aa9b78354206966ddf779658eec50fd
Howard Fan <Howard_Fan@jabil.com>
2015-02-08 17:20:11 +0800

    0
    0 for CLJS too

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,32 @@
-(ns alphabet-cipher.coder)
+(ns alphabet-cipher.coder
+  (:require [clojure.string :as string]))
 
-(defn encode [keyword message]
-  "encodeme")
+(def codebook "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")
+(count (distinct codebook))
 
-(defn decode [keyword message]
-  "decodeme")
+(defn -secret
+  "take first n chars by repeating k many times as necessary"
+  [n k]
+  (take n (flatten (repeat (seq k)))))
+
+(-secret 5 "ab")
+
+(defn index-a [c]
+  (- (int c) (int \a))) ; CLJ
+  ;(- (.charCodeAt c) (.charCodeAt \a))) ; CLJS
+
+(defn -encode [yes secret message]
+  (let [n (count message)
+        secret (-secret n secret)]
+  (for [i (range n)
+      :let [m (index-a (nth message i))
+            s (index-a (nth secret i))
+            k (if yes (+ s m) (- (+ m 26) s))]]
+        (nth codebook k))))
+
+(defn encode [secret message]
+  (string/join (-encode true secret message)))
+
+(defn decode [secret message]
+  (string/join (-encode false secret message)))
+
